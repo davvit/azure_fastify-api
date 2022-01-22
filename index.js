9f2eb6080941843c9555c74bda869193badbf77e
DawitA <davvit@users.noreply.github.com>
2022-01-21 23:21:04 -0600

    get port from env variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const port = process.env.PORT || 3000
 
 // Declare a route
 fastify.get('/hello', async (request, reply) => {
@@ -29,7 +30,7 @@ fastify.get('/ping', opts, async (request, reply) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(3011)
+        await fastify.listen(port)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
